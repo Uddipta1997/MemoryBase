f0f5cc0397451053c9af0e6d07f000ce60824421
Uddipta1997 <48414531+Uddipta1997@users.noreply.github.com>
2020-08-21 09:51:22 +0530

    Update UploadForm.js

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
+const UploadForm = () => {
 
+    const changeHandler = (e) => {
+        console.log('changed');
+    }
+
+    return(
+        <form>
+            <input type = "file" onChange={changeHandler}/>
+        </form>
+    )
+}
 
 
 export default UploadForm;
